c8f6e8aa3a5084cdc888b571284a67cbaedf4223
stnn00 <nguyens6@csp.edu>
2025-10-04 21:17:47 -0500

    Refactor: fix spacing and blank lines to adhere to PEP 8

--- test_contact_manager.py
+++ test_contact_manager.py
@@ -13,14 +13,14 @@ class TestContactManager(unittest.TestCase):
         self.assertIn("Vanilla", contact_manager.contacts)
         self.assertEqual(contact_manager.contacts["Vanilla"], "612-890-1234")
 
-    
     def test_finding_existing_contact(self):
         """Test finding an existing contact."""
         contact_manager.contacts.clear()
         contact_manager.add_contact("Anna", "612-123-4567")
 
-        self.assertIn("Anna", contact_manager.contacts)
-        self.assertEqual(contact_manager.contacts["Anna"], "612-123-4567")
+        phone_number = contact_manager.find_contact("Anna")
+
+        self.assertEqual(phone_number, "612-123-4567")
 
 
 if __name__ == "__main__":
